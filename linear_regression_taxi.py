94a50d421d480337a9ae5e40bced0e16cf4f0ba5
Sina Mohammadian <34884156+sinamhdn@users.noreply.github.com>
2025-04-12 20:10:11 +0330

    Created linear_regression_taxi.py

---
+++ linear_regression_taxi.py
@@ -0,0 +1,345 @@
+import subprocess
+import sys
+import importlib.util
+
+
+class Install:
+    """
+    Check for packages and install them
+    """
+
+    def __init__(self, packages):
+        self.packages_to_check = packages
+
+    def check_packages_installed(self, packages):
+        """
+        Checks if a list of Python packages are installed in the current environment.
+
+        Args:
+        package_list: A list of strings, where each string is the name of a package.
+
+        Returns:
+        A dictionary where keys are package names and values are booleans indicating
+        whether the package is installed (True) or not (False).
+        """
+
+        package_list = [
+            (
+                package.split("~=")[0]
+                if "~=" in package
+                else package.split("=")[0] if "=" in package else package
+            )
+            for package in packages
+        ]
+
+        installed_status = {}
+        for package_name in package_list:
+            installed_status[package_name] = (
+                importlib.util.find_spec(package_name) is not None
+            )
+
+        return installed_status
+
+    def missing_packages(self):
+        """
+        Returns a list of packages that are not installed.
+        """
+        missing = []
+        installation_results = self.check_packages_installed(self.packages_to_check)
+        for package, is_installed in installation_results.items():
+            print(f"{package}: {'Installed' if is_installed else 'Not Installed'}")
+            if not is_installed:
+                missing.append(package)
+        print(f"\n\nMissing packages: {missing}")
+        return missing
+
+    def install_packages(self):
+        """
+        Install packages that are not installed.
+        """
+        missing = self.missing_packages()
+        if len(missing) != 0:
+            for missed_package in missing:
+                subprocess.check_call(
+                    [sys.executable, "-m", "pip", "install", missed_package]
+                )
+        print("\n\nAll requirements successfully installed.")
+
+
+packages_to_check = [
+    "plotly",
+    "seaborn",
+    "keras~=3.8.0",
+    "matplotlib~=3.10.0",
+    "numpy~=2.0.0",
+    "pandas~=2.2.0",
+    "tensorflow~=2.18.0",
+]
+
+# Install required libraries
+install = Install(packages_to_check)
+install.install_packages()
+
+
+# Load dependencies
+# general
+import io
+
+# data
+import numpy as np
+import pandas as pd
+
+# machine learning
+import keras
+
+# data visualization
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Load Datasets
+# chicago_taxi_dataset = pd.read_csv(
+#     "https://download.mlcc.google.com/mledu-datasets/chicago_taxi_train.csv"
+# )
+chicago_taxi_dataset = pd.read_csv("./datasets/chicago_taxi_train.csv")
+
+
+# Read dataset
+# Updates dataframe to use specific columns.
+training_df = chicago_taxi_dataset[
+    ["TRIP_MILES", "TRIP_SECONDS", "FARE", "COMPANY", "PAYMENT_TYPE", "TIP_RATE"]
+]
+print("Read dataset completed successfully.")
+print("Total number of rows: {0}\n\n".format(len(training_df.index)))
+print("\n\n||||First 200 rows of dataset||||\n\n", training_df.head(200))
+
+# View dataset statistics
+print("Total number of rows: {0}\n\n".format(len(training_df.index)))
+print("\n\n||||Dataset Statistics||||\n\n", training_df.describe(include="all"))
+
+# View answers about dataset statistics
+
+answer = '''
+What is the maximum fare? 				              Answer: $159.25
+What is the mean distance across all trips? 		Answer: 8.2895 miles
+How many cab companies are in the dataset? 		  Answer: 31
+What is the most frequent payment type? 		    Answer: Credit Card
+Are any features missing data? 				          Answer: No
+'''
+
+# You should be able to find the answers to the questions about the dataset
+# by inspecting the table output after running the DataFrame describe method.
+#
+# Run this code cell to verify your answers.
+
+# What is the maximum fare?
+max_fare = training_df['FARE'].max()
+print("What is the maximum fare? \t\t\t\tAnswer: ${fare:.2f}".format(fare = max_fare))
+
+# What is the mean distance across all trips?
+mean_distance = training_df['TRIP_MILES'].mean()
+print("What is the mean distance across all trips? \t\tAnswer: {mean:.4f} miles".format(mean = mean_distance))
+
+# How many cab companies are in the dataset?
+num_unique_companies =  training_df['COMPANY'].nunique()
+print("How many cab companies are in the dataset? \t\tAnswer: {number}".format(number = num_unique_companies))
+
+# What is the most frequent payment type?
+most_freq_payment_type = training_df['PAYMENT_TYPE'].value_counts().idxmax()
+print("What is the most frequent payment type? \t\tAnswer: {type}".format(type = most_freq_payment_type))
+
+# Are any features missing data?
+missing_values = training_df.isnull().sum().sum()
+print("Are any features missing data? \t\t\t\tAnswer:", "No" if missing_values == 0 else "Yes")
+
+#View correlation matrix
+print("\n\n||||Correlation Matrix||||\n\n", training_df.corr(numeric_only = True))
+
+#View answers about the correlation matrix
+
+# Which feature correlates most strongly to the label FARE?
+# ---------------------------------------------------------
+answer = '''
+The feature with the strongest correlation to the FARE is TRIP_MILES.
+As you might expect, TRIP_MILES looks like a good feature to start with to train
+the model. Also, notice that the feature TRIP_SECONDS has a strong correlation
+with fare too.
+'''
+print(answer)
+
+
+# Which feature correlates least strongly to the label FARE?
+# -----------------------------------------------------------
+answer = '''The feature with the weakest correlation to the FARE is TIP_RATE.'''
+print(answer)
+
+# View pairplot to see relationships between features
+sns.pairplot(training_df, x_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"], y_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"])
+plt.show()
+
+# Define plotting functions
+
+def make_plots(df, feature_names, label_name, model_output, sample_size=200):
+
+  random_sample = df.sample(n=sample_size).copy()
+  random_sample.reset_index()
+  weights, bias, epochs, rmse = model_output
+
+  is_2d_plot = len(feature_names) == 1
+  model_plot_type = "scatter" if is_2d_plot else "surface"
+  fig = make_subplots(rows=1, cols=2,
+                      subplot_titles=("Loss Curve", "Model Plot"),
+                      specs=[[{"type": "scatter"}, {"type": model_plot_type}]])
+
+  plot_data(random_sample, feature_names, label_name, fig)
+  plot_model(random_sample, feature_names, weights, bias, fig)
+  plot_loss_curve(epochs, rmse, fig)
+
+  fig.show()
+  return
+
+def plot_loss_curve(epochs, rmse, fig):
+  curve = px.line(x=epochs, y=rmse)
+  curve.update_traces(line_color='#ff0000', line_width=3)
+
+  fig.append_trace(curve.data[0], row=1, col=1)
+  fig.update_xaxes(title_text="Epoch", row=1, col=1)
+  fig.update_yaxes(title_text="Root Mean Squared Error", row=1, col=1, range=[rmse.min()*0.8, rmse.max()])
+
+  return
+
+def plot_data(df, features, label, fig):
+  if len(features) == 1:
+    scatter = px.scatter(df, x=features[0], y=label)
+  else:
+    scatter = px.scatter_3d(df, x=features[0], y=features[1], z=label)
+
+  fig.append_trace(scatter.data[0], row=1, col=2)
+  if len(features) == 1:
+    fig.update_xaxes(title_text=features[0], row=1, col=2)
+    fig.update_yaxes(title_text=label, row=1, col=2)
+  else:
+    fig.update_layout(scene1=dict(xaxis_title=features[0], yaxis_title=features[1], zaxis_title=label))
+
+  return
+
+def plot_model(df, features, weights, bias, fig):
+  df['FARE_PREDICTED'] = bias[0]
+
+  for index, feature in enumerate(features):
+    df['FARE_PREDICTED'] = df['FARE_PREDICTED'] + weights[index][0] * df[feature]
+
+  if len(features) == 1:
+    model = px.line(df, x=features[0], y='FARE_PREDICTED')
+    model.update_traces(line_color='#ff0000', line_width=3)
+  else:
+    z_name, y_name = "FARE_PREDICTED", features[1]
+    z = [df[z_name].min(), (df[z_name].max() - df[z_name].min()) / 2, df[z_name].max()]
+    y = [df[y_name].min(), (df[y_name].max() - df[y_name].min()) / 2, df[y_name].max()]
+    x = []
+    for i in range(len(y)):
+      x.append((z[i] - weights[1][0] * y[i] - bias[0]) / weights[0][0])
+
+    plane=pd.DataFrame({'x':x, 'y':y, 'z':[z] * 3})
+
+    light_yellow = [[0, '#89CFF0'], [1, '#FFDB58']]
+    model = go.Figure(data=go.Surface(x=plane['x'], y=plane['y'], z=plane['z'],
+                                      colorscale=light_yellow))
+
+  fig.add_trace(model.data[0], row=1, col=2)
+
+  return
+
+def model_info(feature_names, label_name, model_output):
+  weights = model_output[0]
+  bias = model_output[1]
+
+  nl = "\n"
+  header = "-" * 80
+  banner = header + nl + "|" + "MODEL INFO".center(78) + "|" + nl + header
+
+  info = ""
+  equation = label_name + " = "
+
+  for index, feature in enumerate(feature_names):
+    info = info + "Weight for feature[{}]: {:.3f}\n".format(feature, weights[index][0])
+    equation = equation + "{:.3f} * {} + ".format(weights[index][0], feature)
+
+  info = info + "Bias: {:.3f}\n".format(bias[0])
+  equation = equation + "{:.3f}\n".format(bias[0])
+
+  return banner + nl + info + nl + equation
+
+print("SUCCESS: defining plotting functions complete.")
+
+# Define ML functions
+
+def build_model(my_learning_rate, num_features):
+  """Create and compile a simple linear regression model."""
+  # Describe the topography of the model.
+  # The topography of a simple linear regression model
+  # is a single node in a single layer.
+  inputs = keras.Input(shape=(num_features,))
+  outputs = keras.layers.Dense(units=1)(inputs)
+  model = keras.Model(inputs=inputs, outputs=outputs)
+
+  # Compile the model topography into code that Keras can efficiently
+  # execute. Configure training to minimize the model's mean squared error.
+  model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=my_learning_rate),
+                loss="mean_squared_error",
+                metrics=[keras.metrics.RootMeanSquaredError()])
+
+  return model
+
+
+def train_model(model, df, features, label, epochs, batch_size):
+  """Train the model by feeding it data."""
+
+  # Feed the model the feature and the label.
+  # The model will train for the specified number of epochs.
+  # input_x = df.iloc[:,1:3].values
+  # df[feature]
+  history = model.fit(x=features,
+                      y=label,
+                      batch_size=batch_size,
+                      epochs=epochs)
+
+  # Gather the trained model's weight and bias.
+  trained_weight = model.get_weights()[0]
+  trained_bias = model.get_weights()[1]
+
+  # The list of epochs is stored separately from the rest of history.
+  epochs = history.epoch
+
+  # Isolate the error for each epoch.
+  hist = pd.DataFrame(history.history)
+
+  # To track the progression of training, we're going to take a snapshot
+  # of the model's root mean squared error at each epoch.
+  rmse = hist["root_mean_squared_error"]
+
+  return trained_weight, trained_bias, epochs, rmse
+
+
+def run_experiment(df, feature_names, label_name, learning_rate, epochs, batch_size):
+
+  print('INFO: starting training experiment with features={} and label={}\n'.format(feature_names, label_name))
+
+  num_features = len(feature_names)
+
+  features = df.loc[:, feature_names].values
+  label = df[label_name].values
+
+  model = build_model(learning_rate, num_features)
+  model_output = train_model(model, df, features, label, epochs, batch_size)
+
+  print('\nSUCCESS: training experiment complete\n')
+  print('{}'.format(model_info(feature_names, label_name, model_output)))
+  make_plots(df, feature_names, label_name, model_output)
+
+  return model
+
+print("SUCCESS: defining linear regression functions complete.")
